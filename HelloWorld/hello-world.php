e7d8b84e9fd2ac84de8ae766f3c745275155dfcf
gustavoghislandi <gugasouza3003@gmail.com>
2026-02-13 22:24:32 -0300

    Aula03

--- HelloWorld/hello-world.php
+++ HelloWorld/hello-world.php
@@ -4,11 +4,17 @@
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Hello World PHP</title>
+    <style>
+        h2 {
+            color: lightblue;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
 </head>
 <body>
     <h1>Testando PHP</h1>
     <?php 
-    echo "Olá, Mundo!";
+    echo "<h2>Olá, Mundo!</h2>";
     ?>
     <br>
         <?php 
