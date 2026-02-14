a9d0bc6880df16a18e3d7496bb3e678c0fd1692e
gustavoghislandi <gugasouza3003@gmail.com>
2026-02-13 22:02:02 -0300

    Olá mundo! Em PHP

---
+++ HelloWorld/hello-world.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang='pt-BR'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Hello World PHP</title>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php 
+    echo "Olá, Mundo!";
+    ?>
+    <br>
+        <?php 
+    echo "Hello, World!";
+    ?>
+</body>
+</html>
